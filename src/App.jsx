98cbbedeaf9f540e3fadbff7480d53c2c2d627ba
aleksandremdivani <aleksandre.mdivani@geolab.edu.ge>
2025-12-29 17:47:14 +0400

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useEffect } from 'react';
-import './App.css'
-import Product from './componets/product';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Product from "./componets/product";
 
 function App() {
-  let products = [];
+  const [products, setProducts] = useState([]);
+  const increaseQuantity = (id) => {
+    setProducts((prev) =>
+      prev.map((item) => {
+        if (item.id === id) {
+          return { ...item, quantity: item.quantity + 1 };
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+  const decreaseQuantity = (id) => {
+      setProducts((prev) => 
+        prev.map((item) => {
+        if(id === item.id & item.quantity > 0){
+          return {...item, quantity: item.quantity - 1}
+        }else{
+          return item;
+        }
+      }));
+  };
+  const removeProduct = (id) => {
+   const filteredList = products.filter((item) => 
+    item.id !== id)
+   setProducts(filteredList);
+  }
+
   useEffect(() => {
     const fetchData = async () => {
-    try {
-      const response = await fetch("https://dummyjson.com/carts/5");
-      const data = await response.json();
-      products = data.products;
-    } catch (error) {
-      console.log("error");
-    }
-  }
-  fetchData();
-  }, [])
-  
+      try {
+        const response = await fetch("https://dummyjson.com/carts/5");
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
-    <main>
-      {products.map((item) => {
-        return (
+    <section className="flex min-h-screen justify-center items-center">
+      <main className="flex flex-wrap h-auto flex-row gap-3 max-w-[900px]">
+        {products.map((product) => (
           <Product
-          key={item.id}
-          title={item.title}
-          quantity={item.quantity}/>
-        )
-      })}
-    </main>
-  )
+            key={product.id}
+            title={product.title}
+            quantity={product.quantity}
+            image={product.thumbnail}
+            increase={() => increaseQuantity(product.id)}
+            decrease={() => decreaseQuantity(product.id)}
+            remove={() => removeProduct(product.id)}
+          />
+        ))}
+      </main>
+    </section>
+  );
 }
 
-export default App
+export default App;
--- src/componets/Product.jsx
+++ src/componets/Product.jsx
@@ -1,11 +1,15 @@
-const Product = ({ title, quantity, image }) => (
-  <div>
-    <h3>{title}</h3>
+const Product = ({ title, quantity, image, decrease, increase, remove }) => (
+  <div className="border w-[31%] h-[400px] p-2 flex flex-col gap-y-[40px] items-center">
     <div>
-        <button>-</button>
-        <p>{quantity}</p>
-        <button>+</button>
+    <h3 className="font-bold">{title}</h3>
+    <img src={image} alt={title} />
     </div>
+    <div className="flex gap-x-[10px]">
+        <button onClick={decrease} className="border w-10">-</button>
+        <p>{quantity}</p>
+        <button onClick={increase} className="border w-10">+</button>
+    </div>
+    <button onClick={remove} className="border p-3 w-[45px] rounded-md">x</button>
   </div>
 );
 
